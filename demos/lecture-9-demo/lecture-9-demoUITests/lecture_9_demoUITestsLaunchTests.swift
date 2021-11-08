0c6819811c39c9a4a6514d8b9b51823364855130
Misty Liao <60199324+mistyliao@users.noreply.github.com>
2021-11-08 18:56:22 -0500

    Add files via upload

---
+++ demos/lecture-9-demo/lecture-9-demoUITests/lecture_9_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lecture_9_demoUITestsLaunchTests.swift
+//  lecture-9-demoUITests
+//
+//  Created by Misty Liao on 11/7/21.
+//
+
+import XCTest
+
+class lecture_9_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
